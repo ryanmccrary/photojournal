02a368e3555e1e3230fabc8ee4dfd60c8b7ea5d4
Ryan McCrary <rrmccrary@gmail.com>
2013-02-08 12:06:15 -0500

    Initial commit - created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photojournal::Application.config.secret_token = '45abb0dc15787e76271d0050be12ce86c610d1d820f7333b7ddccc6f42b89e99cfada50c5c2a2107f9080a50df0c599f479104b5b05da384f5c2516aa6d2e994'
