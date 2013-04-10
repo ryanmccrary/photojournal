289d3413571ca048967d50258936f4a34df01620
Ryan McCrary <rrmccrary@gmail.com>
2013-04-10 18:15:43 -0400

    It works

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'aws',
+    :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAIF3NQVKURBR2R2FQ',
     :aws_secret_access_key  => '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',
     :region                 => 'us-east-1'
